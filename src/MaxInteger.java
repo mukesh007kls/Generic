e07cd688e5ed37e6693f67b4262d79587797695f
mukesh007kls <mallikanti2000@gmail.com>
2023-03-07 18:44:35 +0530

    ADDED:Code find max of three numbers

---
+++ src/MaxInteger.java
@@ -0,0 +1,16 @@
+public class MaxInteger {
+    public static <T extends Comparable<T>> void findMax(T x,T y,T z){
+        T max=x;
+        if(y.compareTo(max)>0){
+            max=y;
+        }
+        if(z.compareTo(max)>0){
+            max=z;
+        }
+        System.out.println("Max number is:-"+max);
+    }
+    public static void main(String[] args) {
+        Integer x=20,y=10,z=14;
+        findMax(x,y,z);
+    }
+}
